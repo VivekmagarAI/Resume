8d06187de8e3068979fe4c3bb6f9c0c3e86fa24c
VivekmagarAI <vivekmagar@gmail.com>
2025-03-13 13:37:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function loadTab(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => document.getElementById('content').innerHTML = data)
+        .catch(error => console.error('Error loading page:', error));
+}
